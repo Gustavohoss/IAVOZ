0e2f340eb050b808c9e28fb60d47b6d314b327c0
Gustavo Henrique <ghkueigamer120@gmail.com>
2026-05-18 13:18:05 +0000

    Quero essa imagem para, favicon, foto do webapp:
    https://s3.typebotstor

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 's3.typebotstorage.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
